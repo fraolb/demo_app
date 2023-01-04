c1942c1fb2c6f52463497677f18dc9f36d78daa5
fraolb <Fraolbereket@gmail.com>
2023-01-04 12:29:00 +0300

    ch

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1600,
+    chunkSizeWarningLimit: 1700,
   },
   plugins: [react()],
 })
